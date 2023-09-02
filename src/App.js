7f6d85de34808a4d6ec70fc1dc4572f22fb06c68
erdemburak <burakerdem6@hotmail.com>
2023-09-02 16:17:35 +0300

    Multiple Components

--- src/App.js
+++ src/App.js
@@ -3,21 +3,11 @@ import Navbar from './Navbar';
 import Home from './Home';
 
 function App() {
-  const title = 'Welcome to the new ToDo List';
-  const likes = 50;
-  const link = "http://www.google.com";
-
-
   return (
     <div className="App">
       <Navbar />
       <div className="content">
-        <h1>{title}</h1>
-        <p>{likes}</p>
-        <p>{ "Hello hello"}</p>
-        <p>{100} </p>
-
-        <a href={link}>Google</a>
+        <Home />
       </div>
 
     </div>
