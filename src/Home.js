3d99d0ad19cee670528246e384657e243777117a
erdemburak <burakerdem6@hotmail.com>
2023-09-02 17:18:57 +0300

    Click Events

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,18 @@
 const Home = () => {
+
+    const handleClick = () => {
+        console.log('Hello');
+    }
+
+    const handleClickAgain = (name) => {
+        console.log('Hello ' + name);
+    }
+
     return ( 
         <div className="home">
             <h2>HomePage</h2>
+            <button onClick={handleClick}>Click me</button>
+            <button onClick={() => handleClickAgain('Armağan')}>Click me again</button>
         </div>
      );
 }
