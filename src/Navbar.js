2d90889027ac5d571fd551a393a7e6cafc0b35f5
erdemburak <burakerdem6@hotmail.com>
2023-09-02 16:13:14 +0300

    Started To React

---
+++ src/Navbar.js
@@ -0,0 +1,13 @@
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <h1>The ToDo List</h1>
+            <div className="links">
+                <a href="/">Home</a>
+                <a href="/create">New List</a>
+            </div>
+        </nav>
+    );
+}
+ 
+export default Navbar;
