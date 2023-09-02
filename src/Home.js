edd0dac2998e945f842b81b07bca276e8a087a66
erdemburak <burakerdem6@hotmail.com>
2023-09-02 20:36:26 +0300

    useEffect usage

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState,useEffect } from 'react';
 import BlogList from './BlogList';
 
 const Home = () => {
@@ -8,14 +8,22 @@ const Home = () => {
         {title: 'Wev dev top tips', body: 'lorem ipsum...', author: 'armaganim', id: 3}
     ]);
 
+    const [name, setName] = useState('burak');
+
     const handleDelete = (id) => {
         const newBlogs = blogs.filter(blog => blog.id !== id);
         setBlogs(newBlogs);
     }
 
+    useEffect(() => {
+        console.log('use effect ran');
+    },[name]);
+
     return ( 
         <div className="home">
             <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete}/>
+            <button onClick={() => setName('armaganim')}>Change Name</button>
+            <p>{name}</p>
         </div>
      );
 }
